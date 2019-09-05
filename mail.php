0a54d21f7fe0a653bd5f87dab810f3cee9c08b68
Joydeep Sengupta <joydeep.roni@gmail.com>
2019-09-05 23:40:09 +0530

    added contact

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Origin: *");
                 $name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["con_email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["con_message"]);
- 
+
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
@@ -15,21 +15,21 @@ header("Access-Control-Allow-Origin: *");
             echo "Please complete the form and try again.";
             exit;
         }
- 
+
         // Set the recipient email address.
-        $recipient = "test@hasthemes.com";
- 
+        $recipient = "aamir@wallify.in";
+
         // Set the email subject.
-        $subject = "Test Email for Template Demo - Mail From $name";
- 
+        $subject = "Email from Wallify Website $name";
+
         // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
         $email_content .= "Message:\n$message\n";
- 
+
         // Build the email headers.
         $email_headers = "From: $name <$email>";
- 
+
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
@@ -40,11 +40,11 @@ header("Access-Control-Allow-Origin: *");
             http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
- 
+
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
- 
+
 ?>
